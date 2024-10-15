4504887c8ac4c810eafa8e03c11e6aa051c4569e
Julienpetit951 <julienpetit951@gmail.com>
2024-10-15 20:11:05 +0200

    Ajout des setters
    Ajout des setters

--- Paiement.js
+++ Paiement.js
@@ -6,9 +6,9 @@ class Paiement {
     #interet = 0.03
 
     constructor(solde, depot, retrait) {
-        /*this.#solde = solde
+        this.#solde = solde
         this.#depot = depot
-        this.#retrait = retrait*/
+        this.#retrait = retrait
     }
     //Faire ma logique pour chaque dépot ici
     depot(){
@@ -18,28 +18,48 @@ class Paiement {
 
     //Faire ma logique pour chaque retrait ici
     retrait(){
-        //Ajouter la condition d'avoir un solde suffisant
+        if (this.#solde >= this.#retrait) {
         this.#solde = this.#solde - this.#retrait
         console.log("Vous avez retiré " + this.#retrait + " euros. Nouveau solde : " + this.#solde + "euros.")
+        } else {
+          console.log("Solde insuffisant pour effectuer ce retrait.")} 
     }
 
+    //Calculer l'interêt sur le solde
     interet(){ 
         this.#solde = this.#solde * this.#interet
         console.log("L’interet est de " + this.#interet + " euros. Nouveau solde : " + this.#solde + "euros.")
     }
 
+    //Récupérer le solde
     get solde() {
         return this.#solde;
       }
+    //Récupérer le dépot
     get depot() {
       return this.#depot;
     }
+    //Récupérer le retrait
     get retrait() {
       return this.#retrait;
     }
-
+    //Récupérer le taux d'interêt (Utile dans ce cas ci ?)
     get interet() {
         return this.#interet;
+      }
+
+    // Setters
+    set solde(solde) {
+        this.#solde = solde;
+      }
+    set depot(depot) {
+      this.#depot = depot;
+    }
+    set retrait(retrait) {
+      this.#retrait = retrait;
+    }
+    set interet(interet) {
+        this.#interet = interet;
       }
 
 }
