37b1892b90db69ce7c888bbe5557c82334ba55ed
Julienpetit951 <julienpetit951@gmail.com>
2024-10-15 19:50:12 +0200

    Ajout des commentaires

--- Paiement.js
+++ Paiement.js
@@ -6,20 +6,26 @@ class Paiement {
     #interet = 0.03
 
     constructor(solde, depot, retrait) {
-        this.#solde = solde
+        /*this.#solde = solde
         this.#depot = depot
-        this.#retrait = retrait
+        this.#retrait = retrait*/
     }
-    //Faire ma logique pour mo dépot ici
+    //Faire ma logique pour chaque dépot ici
     depot(){
         this.#solde = this.#solde + this.#depot
         console.log("Vous avez déposé " + this.#depot + " euros. Nouveau solde : " + this.#solde + "euros.")
     }
 
+    //Faire ma logique pour chaque retrait ici
     retrait(){
         //Ajouter la condition d'avoir un solde suffisant
         this.#solde = this.#solde - this.#retrait
         console.log("Vous avez retiré " + this.#retrait + " euros. Nouveau solde : " + this.#solde + "euros.")
+    }
+
+    interet(){ 
+        this.#solde = this.#solde * this.#interet
+        console.log("L’interet est de " + this.#interet + " euros. Nouveau solde : " + this.#solde + "euros.")
     }
 
     get solde() {
@@ -31,6 +37,10 @@ class Paiement {
     get retrait() {
       return this.#retrait;
     }
+
+    get interet() {
+        return this.#interet;
+      }
 
 }
 
